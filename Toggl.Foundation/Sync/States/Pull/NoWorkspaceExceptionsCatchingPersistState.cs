6935db8088dc4de8d81445655029e7378e0f0a08
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2018-06-05 23:02:39 +0300

    ✨ Add NoWorkspaceExceptionsCatching

---
+++ Toggl.Foundation/Sync/States/Pull/NoWorkspaceExceptionsCatchingPersistState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using Toggl.Foundation.Exceptions;
+using Toggl.Foundation.Sync.States.Pull;
+using Toggl.Multivac;
+using Toggl.Multivac.Models;
+
+namespace Toggl.Foundation.Sync.States
+{
+    internal sealed class NoWorkspaceExceptionsCatchingPersistState : IPersistState
+    {
+        private readonly IPersistState internalState;
+
+        public StateResult<IFetchObservables> FinishedPersisting { get; } = new StateResult<IFetchObservables>();
+
+        public StateResult<Exception> Failed { get; } = new StateResult<Exception>();
+
+        public NoWorkspaceExceptionsCatchingPersistState(IPersistState internalState)
+        {
+            Ensure.Argument.IsNotNull(internalState, nameof(internalState));
+
+            this.internalState = internalState;
+        }
+
+        public IObservable<ITransition> Start(IFetchObservables fetch)
+            => fetch.GetList<IWorkspace>()
+                .SelectMany(workspaces => workspaces.Any() 
+                    ? handlePresenceOfWorkspaces(fetch)
+                    : Observable.Throw<ITransition>(new NoWorkspaceException()));
+
+        private IObservable<ITransition> handlePresenceOfWorkspaces(IFetchObservables fetch)
+            => internalState.Start(fetch)
+                .Select(_ => FinishedPersisting.Transition(fetch))
+                .Catch((Exception exception) => processError(exception));
+
+        private IObservable<ITransition> processError(Exception exception)
+            => Observable.Return(Failed.Transition(exception));
+    }
+}
